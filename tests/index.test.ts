e83a24e659702c4c4f614767427f7c9f1651388c
Michael James <micrjames921@gmail.com>
2024-01-21 09:49:34 -0700

    completed tests for Form class

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -20,7 +20,6 @@ describe("Form.", () => {
 	  document = dom.window.document;
 	  input_names = ["name", "username", "password", "confirm_password", "email"];
 	  login = new Form(document.forms[0], data => {
-	     console.log(data);
 	  });
 	  submitBtn = login.submitBtn;
 	  input_names.forEach((input_name, i) => {
@@ -29,37 +28,95 @@ describe("Form.", () => {
 	  fireEvent.click(submitBtn);
    });
    describe("A Form in the document.", () => {
+	  test("Should be defined.", () => {
+		 expect(login.form).toBeDefined();
+	  });
 	  test("Should be in the document.", () => {
 		 expect(login.form).toBeInTheDocument();
 	  });
    });
    describe("A Form with the correct structure.", () => {
-	  test("Should have more than one input.", () => {
-		 expect(login.inputs).toHaveLength(5);
+	  describe("A Form with correctly structured inputs.", () => {
+		 test("Should have each input be defined.", () => {
+			login.inputs.forEach(input => {
+			   expect(input).toBeDefined();
+			});
+		 });
+		 test("Should have more than one input.", () => {
+			expect(login.inputs).toHaveLength(5);
+		 });
+		 test("Should have as many inputs as name attributes.", () => {
+			expect(login.inputs).toHaveLength(input_names.length);
+		 });
+		 test("Should have name attributes with matching values.", () => {
+			expect(login.inputsNames).toHaveLength(input_names.length);
+		 });
 	  });
-	  test("Should have as many inputs as name attributes.", () => {
-		 expect(login.inputs).toHaveLength(input_names.length);
-	  });
-	  test("Should have name attributes with matching values.", () => {
-		 expect(login.inputsNames).toHaveLength(input_names.length);
-	  });
-	  test("Should have a submit button.", () => {
-		 expect(submitBtn).toBeInTheDocument();
-	  });
-	  test("Should have only one submit button.", () => {
-		 let submitBtns: HTMLButtonElement[] = [];
-		 for(let i = 0; i < login.btns.length; i++)
-		 	if(login.btns[i].type == "submit")
-			   submitBtns = [...submitBtns, login.btns[i]];
-		 expect(submitBtns.length == 1).toBeTruthy();
+	  describe("A Form with correctly structured output control.", () => {
+		 test("Should have a submit button that is defined.", () => {
+			expect(submitBtn).toBeDefined();
+		 });
+		 test("Should have a submit button.", () => {
+			expect(submitBtn).toBeInTheDocument();
+		 });
+		 test("Should have only one submit button.", () => {
+			let submitBtns: HTMLButtonElement[] = [];
+			for(let i = 0; i < login.btns.length; i++)
+			   if(login.btns[i].type == "submit")
+				  submitBtns = [...submitBtns, login.btns[i]];
+			expect(submitBtns.length == 1).toBeTruthy();
+		 });
 	  });
    });
    describe("A Form with the correct output.", () => {
-	  test("Should not output an empty object.", () => {
+	  describe("A Form with the correct output should have that output.", () => {
+		 test("Should be defined.", () => {
+			expect(login.data).toBeDefined();
+		 });
+		 test("Should output the data as an object.", () => {
+			expect(typeof login.data).toBe("object");
+		 });
+		 test("Should not output an empty object.", () => {
+			expect(login.data).not.toEqual({});
+		 });
+		 test("Should not have any fields that are empty.", () => {
+			Object.keys(login.data).forEach(field => {
+			   expect(login.data[field]).not.toBeFalsy();
+			});
+		 });
 	  });
-	  test.todo("Should output as many fields as there are inputs.");
-	  test.todo("Should not have any fields that are empty.");
-	  test.todo("Should output the data as an object.");
-	  test.todo("Should output as an object of name/value pairs.");
+	  describe("A Form with the correct output should have the correct structure.", () => {
+		 test("Should output as many fields as there are inputs.", () => {
+			const objArr = Object.keys(login.data);
+			const arrSize = input_names.length;
+			expect(objArr).toHaveLength(arrSize);
+		 });
+		 test("Should output the data as an object.", () => {
+			expect(typeof login.data).toBe("object");
+		 });
+		 test("Should output as an object of name/value pairs.", () => {
+			expect(login.data).toEqual(
+				expect.objectContaining({
+					name: expect.any(String),
+					username: expect.any(String),
+					password: expect.any(String),
+					confirm_password: expect.any(String),
+					email: expect.any(String)
+				})
+			);
+		 });
+	  });
+	  describe("A Form with the correct output should output correctly.", () => {
+		 test("Should have the correct output properties.", () => {
+			Object.keys(login.data).forEach((field, i) => {
+			   expect(field).toEqual(input_names[i]);
+			});
+		 });
+		 test("Should output what was input.", () => {
+			Object.keys(login.data).forEach((field, i)  => {
+			   expect(login.data[field]).toEqual(login.values[i]);
+			});
+		 });
+	  });
    });
 });
